892889b2321dcb7e38d2b8f3ac3960d094a40fde
Javier-Gifkins <javier-gifkins@live.nmit.ac.nz>
2025-05-05 21:14:31 +1200

    Fixing binary thing(but not really)

--- binaryCalculator.js
+++ binaryCalculator.js
@@ -1,14 +1,41 @@
-const choice = prompt("Type 'bin' to convert binary to decimal. Type 'dec' to convert decimal to binary");
-const input = prompt("Enter you Number: ");
+// const choice = prompt("Type 'bin' to convert binary to decimal. Type 'dec' to convert decimal to binary");
+// const input = prompt("Enter you Number: ");
 
-if (choice === "bin") {
-    let decimal = parseInt(input, 2);
-    console.log(`${choice} in Decimal form is ${decimal}.`)
+// if (choice === "bin") {
+//     let decimal = parseInt(input, 2);
+//     console.log(`${choice} in Decimal form is ${decimal}.`)
     
-} else if (choice = "dec") {
-    let binary = Number(input).toString(2);
-    console.log(`${choice} in Binary form is ${binary}.`)
-    
-} else {
-    console.log("Invalid Choice.")
-}
+// } else if (choice = "dec") {
+//     let binary = Number(input).toString(2);
+//     console.log(`${choice} in Binary form is ${binary}.`)
+
+// } else {
+//     console.log("Invalid Choice.")
+// }
+
+// console.log("Hello, world!");
+
+// ES5
+
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Type 'bin' to convert binary to decimal. Type 'dec' to convert decimal to binary: ", function(choice) {
+    rl.question("Enter your number: ", function(input) {
+        if (choice === "bin") {
+            var decimal = parseInt(input, 2);
+            console.log("Binary in Decimal form is " + decimal + ".");
+        } else if (choice === "dec") {
+            var binary = Number(input).toString(2);
+            console.log("Decimal in Binary form is " + binary + ".");
+        } else {
+            console.log("Invalid Choice.");
+        }
+        rl.close();
+    });
+});
+
